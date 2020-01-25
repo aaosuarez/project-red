09aba32aa8066d7f76053b375f972559678d64d3
Aaron Suarez <aaosuarez@gmail.com>
2020-01-24 23:21:07 -0800

    Initial Commit

---
+++ Project Red/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Red
+//
+//  Created by Aaron Suarez on 1/24/20.
+//  Copyright © 2020 Aaron Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
